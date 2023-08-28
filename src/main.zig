74fc775d96bab847ecfb2962a45df7a3ba5210c9
Gabriel Santos <gabrielopesantos97@gmail.com>
2023-08-28 23:54:45 +0100

    use gp allocator instead of arena allocator and setup logger

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const os = std.os;
 
-var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+const logger = std.log.scoped(.zlvs_main);
+
+var log_level: std.log.Level = .info;
 
 fn read_connection_sock_msg(socket_fd: os.socket_t, allocator: std.mem.Allocator) void {
     var msg_buf = allocator.alloc(u8, 256) catch |err| switch (err) {
@@ -14,15 +16,19 @@ fn read_connection_sock_msg(socket_fd: os.socket_t, allocator: std.mem.Allocator
 
     var bytes_read = os.read(socket_fd, msg_buf);
     if (bytes_read) |value| {
-        std.debug.print("Successfuly read {d} bytes\n", .{value});
-        std.debug.print("Message: '{s}'\n", .{msg_buf[0..value]});
+        logger.debug("Successfuly read {d} bytes", .{value});
+        logger.debug("Message: '{s}'", .{msg_buf[0..value]});
     } else |err| switch (err) {
         else => std.debug.print("Got an error: {}\n", .{err}),
     }
 }
 
 pub fn main() !void {
-    defer arena.deinit();
+    if (std.mem.eql(u8, std.os.getenv("DEBUG") orelse "", "1")) log_level = .debug;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
     var address = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
 
@@ -33,17 +39,15 @@ pub fn main() !void {
     try os.bind(socket, &address.any, address.getOsSockLen());
 
     try os.listen(socket, 12);
+    logger.info("Server listening on address: 127.0.0.1:{s}", .{"8080"});
 
     var socket_len = address.getOsSockLen();
     const conn_socket = try os.accept(socket, &address.any, &socket_len, os.linux.SOCK.NONBLOCK);
 
-    var thread_pool_allocator = arena.allocator();
-
     // Init thread pool
     var thread_pool: std.Thread.Pool = undefined;
-    try thread_pool.init(.{ .allocator = thread_pool_allocator, .n_jobs = 4 });
+    try thread_pool.init(.{ .allocator = allocator, .n_jobs = 4 });
     defer thread_pool.deinit();
 
-    var allocator = arena.allocator();
     _ = try thread_pool.spawn(read_connection_sock_msg, .{ conn_socket, allocator });
 }
