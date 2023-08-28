5596740f4573dd32f12673ebc0a795f9273bcd87
Gabriel Santos <gabrielopesantos97@gmail.com>
2023-08-29 00:26:01 +0100

    parse input arguments and and a srv_listen function

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const logger = std.log.scoped(.zlvs_main);
 var log_level: std.log.Level = .info;
 
 fn read_connection_sock_msg(socket_fd: os.socket_t, allocator: std.mem.Allocator) void {
+    defer os.closeSocket(socket_fd);
     var msg_buf = allocator.alloc(u8, 256) catch |err| switch (err) {
         else => {
             std.debug.print("Could not allocate buffer: {}\n", .{err});
@@ -23,6 +24,18 @@ fn read_connection_sock_msg(socket_fd: os.socket_t, allocator: std.mem.Allocator
     }
 }
 
+fn srv_listen(srv_sockaddr: *std.net.Address) !std.os.socket_t {
+    // NOTE: socket_type
+    const socket = try os.socket(os.linux.AF.INET, os.linux.SOCK.STREAM, 0);
+
+    try os.bind(socket, &srv_sockaddr.any, srv_sockaddr.getOsSockLen());
+
+    try os.listen(socket, 12); // 12?
+    logger.info("Server listening on address: 127.0.0.1:{d}", .{srv_sockaddr.in.getPort()});
+
+    return socket;
+}
+
 pub fn main() !void {
     if (std.mem.eql(u8, std.os.getenv("DEBUG") orelse "", "1")) log_level = .debug;
 
@@ -30,24 +43,26 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var address = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    // NOTE: socket_type
-    const socket = try os.socket(os.linux.AF.INET, os.linux.SOCK.STREAM, 0);
+    if (args.len != 2) return logger.warn("Invalid number of paramenters, {d}, passed.\nExpected usage: zlvs PORT.", .{args.len});
+
+    const port = try std.fmt.parseInt(u16, args[1], 10);
+
+    var sockaddr = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, port);
+    var socket = try srv_listen(&sockaddr);
     defer os.closeSocket(socket);
 
-    try os.bind(socket, &address.any, address.getOsSockLen());
-
-    try os.listen(socket, 12);
-    logger.info("Server listening on address: 127.0.0.1:{s}", .{"8080"});
-
-    var socket_len = address.getOsSockLen();
-    const conn_socket = try os.accept(socket, &address.any, &socket_len, os.linux.SOCK.NONBLOCK);
+    var sockaddr_size = sockaddr.getOsSockLen();
 
     // Init thread pool
     var thread_pool: std.Thread.Pool = undefined;
     try thread_pool.init(.{ .allocator = allocator, .n_jobs = 4 });
     defer thread_pool.deinit();
 
-    _ = try thread_pool.spawn(read_connection_sock_msg, .{ conn_socket, allocator });
+    while (true) {
+        const conn_socket = try os.accept(socket, &sockaddr.any, &sockaddr_size, os.linux.SOCK.NONBLOCK);
+        _ = try thread_pool.spawn(read_connection_sock_msg, .{ conn_socket, allocator });
+    }
 }
